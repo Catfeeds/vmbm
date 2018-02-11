19dd71d30ba069d99102d0f78da165a407f9344a
SilentLittleCat <13438082119@163.com>
2018-02-11 10:55:54 +0800

    微信配置

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,13 +11,32 @@ class TestController extends Controller
     {
         $buttons = [
             [
-                "type" => "view",
-                "name" => "纸妹子",
-                "url"  => "http://47.104.165.93/user"
-            ]
+                "type" => "click",
+                "name" => "今日歌曲",
+                "key"  => "V1001_TODAY_MUSIC"
+            ],
+            [
+                "name"       => "菜单",
+                "sub_button" => [
+                    [
+                        "type" => "view",
+                        "name" => "搜索",
+                        "url"  => "http://www.soso.com/"
+                    ],
+                    [
+                        "type" => "view",
+                        "name" => "视频",
+                        "url"  => "http://v.qq.com/"
+                    ],
+                    [
+                        "type" => "click",
+                        "name" => "赞一下我们",
+                        "key" => "V1001_GOOD"
+                    ],
+                ],
+            ],
         ];
         $app = app('wechat.official_account');
-        $app->menu->delete();
         $app->menu->create($buttons);
         return 'ok';
     }
