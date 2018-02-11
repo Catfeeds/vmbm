e2f40fe9c730ceafb8be166fbf1640eb5e48d2cd
SilentLittleCat <13438082119@163.com>
2018-02-11 17:57:53 +0800

    数据库更新

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -17,9 +17,9 @@ class WeChatController extends Controller
 //        Log::info('request arrived.');
         $app = app('wechat.official_account');
 
-        $app->server->push(function($message){
-            return 'success';
-        });
+//        $app->server->push(function($message){
+//            return 'success';
+//        });
 
 //        Log::info('return response.');
         return $app->server->serve();
