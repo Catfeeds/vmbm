cac8493006c06aa27ea03cfa5dbd1ccf32797346
SilentLittleCat <13438082119@163.com>
2018-02-11 14:29:30 +0800

    微信配置

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -17,8 +17,8 @@ class WeChatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $app->server->push(function($message){
-            Log::info($message);
+        $app->server->push(function($message) {
+            // Log::info($message);
             return "欢迎关注 overtrue！";
         });
 
