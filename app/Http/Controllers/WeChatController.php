282d81c17abab994562517aab35a26119ed48a4e
SilentLittleCat <13438082119@163.com>
2018-02-11 15:42:43 +0800

    微信配置

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -16,35 +16,10 @@ class WeChatController extends Controller
     {
         Log::info('request arrived.');
         $app = app('wechat.official_account');
-        $app->server->setMessageHandler(function($message) use ($app){
-            if ($message->MsgType=='event') {
-                $user_openid = $message->FromUserName;
-                if ($message->Event=='subscribe') {
-                $user_info['unionid'] = $message->ToUserName;
-                $user_info['openid'] = $user_openid;
-                $userService = $app->user;
-                $user = $userService->get($user_info['openid']);
-                $user_info['subscribe_time'] = $user['subscribe_time'];
-                $user_info['nickname'] = $user['nickname'];
-                $user_info['avatar'] = $user['headimgurl'];
-                $user_info['sex'] = $user['sex'];
-                $user_info['province'] = $user['province'];
-                $user_info['city'] = $user['city'];
-                $user_info['country'] = $user['country'];
-                $user_info['is_subscribe'] = 1;
-                if (WxStudent::weixin_attention($user_info)) {
-                    return '欢迎关注';
-                }else{
-                    return '您的信息由于某种原因没有保存，请重新关注';
-                }
-            }else if ($message->Event=='unsubscribe') {
-                if (WxStudent::weixin_cancel_attention($user_openid)) {
-                    return '已取消关注';
-                }
-            }
-            }
-
+        $app->server->push(function($message){
+            return "欢迎关注 overtrue！";
         });
+
 
         Log::info('return response.');
         return $app->server->serve();
